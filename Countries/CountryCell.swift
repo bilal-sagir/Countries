50bba07f7324bfe7809f577bebb2af686d96d994
bilal-sagir <sagir.bilal@icloud.com>
2022-03-27 02:41:31 +0300

    add tableViewCell

---
+++ Countries/CountryCell.swift
@@ -0,0 +1,23 @@
+//
+//  CountryCell.swift
+//  Countries
+//
+//  Created by Bilal on 27.03.2022.
+//
+
+import UIKit
+
+class CountryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Countries/HomeVC.swift
+++ Countries/HomeVC.swift
@@ -9,16 +9,61 @@ import UIKit
 
 class HomeVC: UIViewController {
 
-    var countries = [Country]()
+    @IBOutlet weak var countriesTableView: UITableView!
+    
+    private var countries = [Country]()
+    private let cellSpacing: CGFloat = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Apicall.fetchCountries { countryList in
-            self.countries = countryList
+        Apicall.fetchCountries { [weak self] (countryList) in
+            self!.countries = countryList
+            
+            DispatchQueue.main.async {
+                self!.countriesTableView.reloadData()
+            }
         }
     }
-
-
 }
+
+// MARK: - Delegations and DataSource
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = countriesTableView.dequeueReusableCell(withIdentifier: "cell") as! CountryCell
+        
+        cell.textLabel?.text = countries[indexPath.section].name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("DEBUG: cell selected")
+    }
+    
+
+    
+    
+}
+
 
