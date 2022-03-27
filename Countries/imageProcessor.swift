83bb44a656d05877675622c295bc834a4aae1758
bilal-sagir <sagir.bilal@icloud.com>
2022-03-27 06:19:01 +0300

    add imageProcessor for kf -svg file

---
+++ Countries/imageProcessor.swift
@@ -0,0 +1,25 @@
+//
+//  imageProcessor.swift
+//  Countries
+//
+//  Created by Bilal on 27.03.2022.
+//
+
+import UIKit
+import Kingfisher
+import SVGKit
+
+public struct SVGImgProcessor:ImageProcessor {
+    public var identifier: String = "com.appidentifier.webpprocessor"
+    public func process(item: ImageProcessItem, options: KingfisherParsedOptionsInfo) -> KFCrossPlatformImage? {
+        switch item {
+        case .image(let image):
+            print("already an image")
+            return image
+        case .data(let data):
+            let imsvg = SVGKImage(data: data)
+            return imsvg?.uiImage
+        }
+    }
+}
+
